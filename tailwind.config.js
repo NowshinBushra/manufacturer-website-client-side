db5e90f009a4c82b2c5bd9e269a4a248d58f5475
NowshinBushra <nowshin.bushra399@gmail.com>
2022-05-24 01:17:09 +0600

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#fdc94b",
+          secondary: "#000000",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
