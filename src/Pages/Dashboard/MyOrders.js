9f4e918e27c5bee93875c304e9dda18de52d0473
NowshinBushra <nowshin.bushra399@gmail.com>
2022-05-28 23:16:15 +0600

    load review data

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyOrders = () => {
+
+    const [orders, setOrders] = useState([]);
+    const [user] = useAuthState(auth);
+
+    useEffect(() => {
+        if(user){
+            fetch(`http://localhost:5000/orders?userEmail=${user.email}`)
+            .then(res => res.json())
+            .then(data => setOrders(data));
+        }
+    }, [user])
     return (
         <div>
-            my
+           <p> My orders: {orders.length}</p>
+           <div className="overflow-x-auto">
+  <table className="table w-full">
+    {/* <!-- head --> */}
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Product Name</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+          orders.map(order => <tr>
+            <th>1</th>
+            <td>{order.userName}</td>
+            <td>{order.userEmail}</td>
+            <td>{order.pName}</td>
+          </tr>)
+      }
+      
+      
+    </tbody>
+  </table>
+</div>
         </div>
     );
 };
--- src/Pages/HomePage/AllReviews.js
+++ src/Pages/HomePage/AllReviews.js
@@ -1,24 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Review from './Review';
 
 const AllReviews = () => {
-    const reviews = [
-        {
-            _id:1,
-            name: "Winson Henry",
-            review: "I found this company worthy. I was a little bit skeptical that the low prices were for real, however I must say that my experience has been incredible good. Good job to the team of bike-components!"
-        },
-        {
-            _id:2,
-            name: "Winson Henry",
-            review: "Excelent service along with unbeatable prices, I will truly recommend this site to all who might need to purchase parts for bike. On my case Fernando from customer care line in Spanish sorted out all my questions!"
-        },
-        {
-            _id:3,
-            name: "Winson Henry",
-            review: "Extremely nice and efficient customer service, Super fast and professionally top on everything! Nothing to complain. Well done Bike Components and Sebastian!"
-        }
-    ]
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/review')
+            .then(res => res.json())
+            .then(data => setReviews(data));
+    }, [])
+
+
     return (
         <section>
                 <h2 className='text-4xl font-bold text-center'>Customers Feedback</h2>
